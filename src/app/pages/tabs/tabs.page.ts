58d4a39fc432c03500ad69166f46fed26180c69b
baffioso <baffyx@gmail.com>
2021-10-03 21:58:25 +0200

    Only show projects tab if auth

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SupabaseService } from '@app/services/supabase.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  userAuthenticated$ = this.supabase.authenticated$;
 
-  constructor() {}
+  constructor(
+    private supabase: SupabaseService
+  ) { }
 
 }
