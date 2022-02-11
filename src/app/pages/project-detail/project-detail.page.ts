5789bb75ffa6d7db4c6c682039b1b50bbbe40af8
baffioso <baffyx@gmail.com>
2022-02-11 13:25:40 +0100

    Use ngrx store

--- src/app/pages/project-detail/project-detail.page.ts
+++ src/app/pages/project-detail/project-detail.page.ts
@@ -4,9 +4,12 @@ import { ActivatedRoute } from '@angular/router';
 import { filter, map, mergeMap, switchMap, tap } from 'rxjs/operators';
 import { ProjectStoreService } from '@app/stores/project-store.service';
 import { SupabaseService } from '@app/services/supabase.service';
-import { forkJoin, from, of } from 'rxjs';
-import { DomSanitizer } from '@angular/platform-browser';
+import { forkJoin, from, Observable, of } from 'rxjs';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { UserNotificationService } from '@app/shared/userNotification.service';
+import { AppState } from '@app/store/app.reducer';
+import { Store } from '@ngrx/store';
+import { ProjectWithRelations } from '@app/interfaces/project';
 
 @Component({
   selector: 'app-project-detail',
@@ -19,43 +22,43 @@ export class ProjectDetailPage implements OnInit {
   selectedTab: 'detail' | 'photos' | 'map' = 'detail';
   loader: HTMLIonLoadingElement;
 
-  project$ = this.route.paramMap.pipe(
-    switchMap(paramMap => {
-      return this.projectStore.projects$.pipe(
-        map(projects => projects.find(project => project.id === paramMap.get('id')))
-      );
-    })
-  );
+  project$: Observable<ProjectWithRelations>;
+  photos$: Observable<SafeResourceUrl[]>;
 
-  photos$ = this.project$.pipe(
-    tap(async () => {
-      // this.loader = await this.notification.presentLoading('Henter billeder...');
-    }),
-    map(project => project.images.map(image => image.file_name.replace('images/', ''))),
-    switchMap(fileNames => {
-      const p = fileNames.map(fileName => this.supabase.downloadImage(fileName));
-      return forkJoin(p).pipe(
-        map(imageFiles => {
-          return imageFiles.map(image => {
-            return this.domSanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(image.data));
-          });
-        })
-      );
-    }),
-    tap(async () => {
-      // this.loader.dismiss();
-    })
-  );
+
+
 
   constructor(
-    private projectStore: ProjectStoreService,
+    // private projectStore: ProjectStoreService,
     private route: ActivatedRoute,
     private supabase: SupabaseService,
     private readonly domSanitizer: DomSanitizer,
+    private store: Store<AppState>,
     private notification: UserNotificationService
   ) { }
 
   ngOnInit() {
+    this.project$ = this.route.paramMap.pipe(
+      switchMap(paramMap => {
+        return this.store.select('project', 'projects').pipe(
+          map(projects => projects.find(project => project.id === paramMap.get('id')))
+        );
+      })
+    );
+
+    this.photos$ = this.project$.pipe(
+      map(project => project.images.map(image => image.file_name.replace('images/', ''))),
+      switchMap(fileNames => {
+        const p = fileNames.map(fileName => this.supabase.downloadImage(fileName));
+        return forkJoin(p).pipe(
+          map(imageFiles => {
+            return imageFiles.map(image => {
+              return this.domSanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(image.data));
+            });
+          })
+        );
+      })
+    );
   }
 
   onTabChange(event) {
